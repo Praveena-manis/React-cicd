3c1079611ccc6d9e1c6cb8978af4baa1baf622bf
Praveena-manis <143096344+Praveena-manis@users.noreply.github.com>
2025-06-17 08:09:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:'https:github.com/Praveena-manis/React-cicd.git',
+  base:'/React-cicd.git/',
   plugins: [react()],
   test: {
     globals: true,
